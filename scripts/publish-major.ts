20820c704dd4c777fe7b50af957012a4e00927d9
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-12-22 20:33:02 +0200

    publish major version - undefined

--- scripts/publish-major.ts
+++ scripts/publish-major.ts
@@ -1,5 +1,9 @@
+import inquirer from "inquirer";
+import { commitQuestions } from "../assets/prompts/commit";
+
 const execPublishMinor = require("child_process").execSync;
-const minorMessage = "BUILD - " + process.argv[2];
+const minorMessage =
+  "publish minor version - " + inquirer.prompt([commitQuestions]);
 execPublishMinor(
   'npm run format && npm run lint && npm run test && git add . && git commit -m "' +
     minorMessage +
--- scripts/publish-minor.ts
+++ scripts/publish-minor.ts
@@ -1,5 +1,5 @@
 const execPublishMajor = require("child_process").execSync;
-const majorMessage = "BUILD - " + process.argv[2];
+const majorMessage = "publish major version - " + process.argv[2];
 execPublishMajor(
   'npm run format && npm run lint && npm run test && git add . && git commit -m "' +
     majorMessage +
