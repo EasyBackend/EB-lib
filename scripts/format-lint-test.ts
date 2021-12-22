7c827493c897e854c52035366b0a2504f376473f
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-12-22 22:35:33 +0200

    C:\Users\Koren\Documents\EB-lib\scripts\commit

--- scripts/format-lint-test.ts
+++ scripts/format-lint-test.ts
@@ -1,5 +1,5 @@
-export const execFormatLintTest = require('child_process').execSync
+export const execFormatLintTest = require("child_process").execSync;
 
-execFormatLintTest(`npm run format && npm run lint && npm run test`, {
+execFormatLintTest(`npm run format && npm run lint:ts && npm run test`, {
   stdio: [0, 1, 2],
-})
+});
